84f20744ce97955260c45b21ea1ac8cc9ad949f6
mhadam <michael@hadam.us>
2022-05-19 21:28:26 -0400

    Add connection debug code

--- app/app/db/database.py
+++ app/app/db/database.py
@@ -20,6 +20,11 @@ engine = create_async_engine(UNQUOTED_DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
+async def test_connection():
+    async with engine.begin() as conn:
+        pass
+
+
 async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
